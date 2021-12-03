adf19a20269724200b51f18a3fe98eb40e11708f
caudaganesh <caudaganesh@gmail.com>
2021-12-03 17:26:02 +0700

    test

--- main_2_test.go
+++ main_2_test.go
@@ -6,7 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_doSomething(t *testing.T) {
+func Test_doSomething2(t *testing.T) {
+	t.Skip()
 	t.Run("do something", func(t *testing.T) {
 		got := doSomething()
 		assert.Equal(t, "Hello, world.", got)
