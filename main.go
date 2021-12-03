ec21a6ff836314f0d5b8156af5975edd7abf6d12
caudaganesh <caudaganesh@gmail.com>
2021-12-03 13:35:49 +0700

    test

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	println("Hello, world.")
+}
+
+func doSomething() string {
+	return "Hello, world."
+}
---
+++ main_3_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_doSomething3(t *testing.T) {
+	t.Skip()
+	t.Run("do something", func(t *testing.T) {
+		got := doSomething()
+		assert.Equal(t, "Hello, world.", got)
+	})
+}
